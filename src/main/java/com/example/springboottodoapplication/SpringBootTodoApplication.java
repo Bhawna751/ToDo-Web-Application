66968f9e5b82b35a92251e6917fa23191ad103f9
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-21 13:32:50 +0530

    Basic Todo app created, without edit option

--- src/main/java/com/example/springboottodoapplication/SpringBootTodoApplication.java
+++ src/main/java/com/example/springboottodoapplication/SpringBootTodoApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTodoApplication {
-
+	// Main Spring boot application method
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootTodoApplication.class, args);
 	}
 
